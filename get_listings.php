9fd7279f069ec499786fcd3ef134daafce28e9f4
Danbytronic <danbytronic@protonmail.com>
2024-06-21 22:58:18 -0400

    Add files via upload

---
+++ get_listings.php
@@ -0,0 +1,149 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<listings>
+
+<tv name="Godzilla Attacks!" flv_url="videos/godzilla.mp4" image_url="godzilla.jpg" tags="weird"/>
+<tv name="Steve Carty: Public Brand Study" copyright="Steve Carty" flv_url="videos/public_brand_study.mp4" image_url="public_brand_study.jpg" tags="subversive"/>
+<tv name="Prefuse 73: The End of Biters" copyright="Warp Records, Aleix Pitarch" flv_url="videos/prefuse_biters.mp4" image_url="prefuse_biters.jpg" tags="music"/>
+<tv name="Midfield General: Midfielding (feat. Noel Fielding)" copyright="Midfield General" flv_url="videos/midfield_general.mp4" image_url="midfield_general.jpg" tags="music"/>
+<tv name="The Whitest Boy Alive: Golden Cage" copyright="Mauro Vecchi, The Whitest Boy Alive" flv_url="videos/wba_cage.mp4" image_url="wba_cage.jpg" tags="music"/>
+<tv name="On/Off" copyright="Jacky Lochinghang" flv_url="videos/on_off.mp4" image_url="on_off.jpg" tags="weird"/>
+<tv name="Go Go Ninja Dinosaur" copyright="Colours Are Brighter" flv_url="videos/ninja_dinosaur.mp4" image_url="ninja_dinosaur.jpg" tags="short"/>
+<tv name="Spoon" copyright="Bitter Films" flv_url="videos/Spoon.mp4" image_url="Spoon.jpg" tags="weird"/>
+<tv name="Kitty's Wizard Mirror" copyright="Hudson-Powell" flv_url="videos/kittys_wizard.mp4" image_url="kittys_wizard.jpg" tags="technology"/>
+<tv name="Almost Real" copyright="Cyril Gfeller" flv_url="videos/almost_real.mp4" image_url="almost_real.jpg" tags="short"/>
+<tv name="The Softlightes: Heart Made of Sound" copyright="Kris Moyes, Modular Recordings" flv_url="videos/softlightes_heart.mp4" image_url="softlightes_heart.jpg" tags="music"/>
+<tv name="Theros" copyright="Georgios Cherouvim, Bournemouth NCCA" flv_url="videos/theros.mp4" image_url="theros.jpg" tags="short"/>
+<tv name="Noodle Monster" flv_url="videos/noodle_monster.mp4" image_url="noodle_monster.jpg" tags="weird"/>
+<tv name="DJ Yoda: Wheels (feat. Level 42)" copyright="No-Domain, Sanctuary Records" flv_url="videos/dj_yoda_wheels.mp4" image_url="dj_yoda_wheels.jpg" tags="music"/>
+<tv name="Carnival" flv_url="videos/Carnival.mp4" image_url="Carnival.jpg" tags="weird"/>
+<tv name="Aerosol Suicide" copyright="Espaun 256" flv_url="videos/Aerosol_Suicide.mp4" image_url="Aerosol_Suicide.jpg" tags="short"/>
+<tv name="Video Projection Monumentale" copyright="Uruk" flv_url="videos/monumentale.mp4" image_url="monumentale.jpg" tags="technology"/>
+<tv name="Dark Star" flv_url="videos/dark_star.mp4" image_url="dark_star.jpg" tags="weird"/>
+<tv name="Mr Deja Vu" copyright="Eric Lerner" flv_url="videos/mr_deja_vu.mp4" image_url="mr_deja_vu.jpg" tags="subversive"/>
+<tv name="The Decemberists: Los Angeles, I'm Yours" copyright="Daniel Oeffinger, Kill Rock Stars" flv_url="videos/decemberists_yours.mp4" image_url="decemberists_yours.jpg" tags="music"/>
+<tv name="Laptop Drop" copyright="Ramon & Pedro" flv_url="videos/laptop_drop.mp4" image_url="laptop_drop.jpg" tags="weird"/>
+<tv name="Adam Freeland: We Want Your Soul" copyright="Adam Freeland" flv_url="videos/freeland_soul.mp4" image_url="freeland_soul.jpg" tags="subversive"/>
+<tv name="For Your Height Only" flv_url="videos/Your_Height_Only.mp4" image_url="Your_Height_Only.jpg" tags="weird"/>
+<tv name="Prefuse 73: Half of What" copyright="Warp Records, Ed Holdsworth" flv_url="videos/prefuse_half_what.mp4" image_url="prefuse_half_what.jpg" tags="music"/>
+<tv name="Wildlife" copyright="Karolina Sobecka" flv_url="videos/sobecka_wildlife.mp4" image_url="sobecka_wildlife.jpg" tags="technology"/>
+<tv name="Fragmentary View" copyright="Second To None Works" flv_url="videos/stnw_fragment.mp4" image_url="stnw_fragment.jpg" tags="short"/>
+<tv name="Poo Party" copyright="Andy Sykes" flv_url="videos/poo_party.mp4" image_url="poo_party.jpg" tags="short"/>
+<tv name="Jack Ladder: Black Hole Blues" copyright="Jack Ladder, FatCat Recordings" flv_url="videos/black_hole_blues.mp4" image_url="black_hole_blues.jpg" tags="music"/>
+<tv name="Carlitopolis" copyright="Luis Nieto" flv_url="videos/carlitopolis.mp4" image_url="carlitopolis.jpg" tags="short"/>
+<tv name="Gay Bar" copyright="Saam Farahmand" flv_url="videos/gay_bar.mp4" image_url="gay_bar.jpg" tags="subversive"/>
+<tv name="Human Drums" copyright="Tetsuaki Baba, Eirik Solheim" flv_url="videos/human_drums.mp4" image_url="human_drums.jpg" tags="technology"/>
+<tv name="Ping Pong Ball" copyright="Fu Pok Yan" flv_url="videos/ping_pong_ball.mp4" image_url="ping_pong_ball.jpg" tags="short"/>
+<tv name="Bonobo: Flutter" copyright="Bonobo, Ninja Tune" flv_url="videos/bonobo_flutter.mp4" image_url="bonobo_flutter.jpg" tags="music"/>
+<tv name="HeHe Train" copyright="HeHe" flv_url="videos/hehe_train.mp4" image_url="hehe_train.jpg" tags="technology"/>
+<tv name="Oliver Laric: 787 Cliparts" copyright="Oliver Laric" flv_url="videos/laric_cliparts.mp4" image_url="laric_cliparts.jpg" tags="short"/>
+<tv name="Ed Harcourt: Visit from the Dead Dog" copyright="Ed Harcourt, Heavenly Records" flv_url="videos/ed_harcourt_dog.mp4" image_url="ed_harcourt_dog.jpg" tags="music"/>
+<tv name="Kloss: DelighTable" copyright="Kloss Interactors" flv_url="videos/kloss_delightable.mp4" image_url="kloss_delightable.jpg" tags="technology"/>
+<tv name="CAPs" copyright="Filmakademie Baden-Württemberg" flv_url="videos/caps.mp4" image_url="caps.jpg" tags="short"/>
+<tv name="Ernest Cline: Dance, Monkeys, Dance" copyright="Ernest Cline" flv_url="videos/dance_monkeys.mp4" image_url="dance_monkeys.jpg" tags="subversive"/>
+<tv name="Massive Viral Outbreak" flv_url="videos/Viral_Outbreak.mp4" image_url="Viral_Outbreak.jpg" tags="weird"/>
+<tv name="Hexstatic: Deadly Media" copyright="Hexstatic, Ninja Tune" flv_url="videos/deadly_media.mp4" image_url="deadly_media.jpg" tags="music"/>
+<tv name="PES: Prank Call" copyright="PES, Sarah Phelps" flv_url="videos/pes.mp4" image_url="pes.jpg" tags="weird"/>
+<tv name="Strandbeest" copyright="Theo Jansen, Alexendar Schlichter" flv_url="videos/strandbeest.mp4" image_url="strandbeest.jpg" tags="technology"/>
+<tv name="Readymade FC: Only One" copyright="Ramon & Pedro" flv_url="videos/only_one.mp4" image_url="only_one.jpg" tags="music"/>
+<tv name="Pizza" copyright="Aleksandra Domanovic" flv_url="videos/Pizza.mp4" image_url="Pizza.jpg" tags="weird"/>
+<tv name="Bush for Peace" copyright="Sarah Christman, Jen Simmons" flv_url="videos/bush_peace.mp4" image_url="bush_peace.jpg" tags="subversive"/>
+<tv name="Pikapika: The Lightning Doodle Project" copyright="Tochka" flv_url="videos/pikapika.mp4" image_url="pikapika.jpg" tags="technology"/>
+<tv name="An Eye for Annai" copyright="Dan Rodrigues, Jon Klassen" flv_url="videos/annai.mp4" image_url="annai.jpg" tags="short"/>
+<tv name="Pictures of Walls" flv_url="videos/pictures_of_walls.mp4" image_url="pictures_of_walls.jpg" tags="subversive"/>
+<tv name="Camera: Out on the Water" copyright="Camera" flv_url="videos/camera_water.mp4" image_url="camera_water.jpg" tags="music"/>
+<tv name="bit.fall aquadisplay" copyright="Julius Popp" flv_url="videos/bit_fall.mp4" image_url="bit_fall.jpg" tags="technology"/>
+<tv name="Monster" copyright="Jan Mathias Steinforth" flv_url="videos/monster.mp4" image_url="monster.jpg" tags="weird"/>
+<tv name="Diversion" copyright="Aidan Gibbons, Simon Reeves" flv_url="videos/diversion.mp4" image_url="diversion.jpg" tags="short"/>
+<tv name="Humans!" copyright="Three Legged Legs" flv_url="videos/humans.mp4" image_url="humans.jpg" tags="subversive"/>
+<tv name="Animated Minds: Fish on a Hook" copyright="Animated Minds" flv_url="videos/fish_hook.mp4" image_url="fish_hook.jpg" tags="short"/>
+<tv name="The Erogenous Zone" copyright="Nice Media" flv_url="videos/erogenous_zone.mp4" image_url="erogenous_zone.jpg" tags="weird"/>
+<tv name="Electroland" copyright="Electroland" flv_url="videos/electroland.mp4" image_url="electroland.jpg" tags="technology"/>
+<tv name="Hello, Dave?" copyright="Hexstatic" flv_url="videos/hello_dave.mp4" image_url="hello_dave.jpg" tags="weird"/>
+<tv name="Banksy Advert" copyright="Banksy" flv_url="videos/banksy.mp4" image_url="banksy.jpg" tags="subversive"/>
+<tv name="Eavesdripping" copyright="Sascha Pohflepp" flv_url="videos/Eavesdripping.mp4" image_url="Eavesdripping.jpg" tags="technology"/>
+<tv name="Grand National: Drink To Moving On" copyright="Grand National" flv_url="videos/drink_moving.mp4" image_url="drink_moving.jpg" tags="music"/>
+<tv name="Crash Ballet" flv_url="videos/crash_ballet.mp4" image_url="crash_ballet.jpg" tags="short"/>
+<tv name="kkssee" copyright="Real DV Prod" flv_url="videos/kkssee.mp4" image_url="kkssee.jpg" tags="weird"/>
+<tv name="Hanne Hukkelberg: A Cheater's Armoury" copyright="Hanne Hukkelberg, Rykestrasse 68" flv_url="videos/cheaters_armoury.mp4" image_url="cheaters_armoury.jpg" tags="music"/>
+<tv name="DFORM: Trash Totem" copyright="DFORM" flv_url="videos/trash_totem.mp4" image_url="trash_totem.jpg" tags="short"/>
+<tv name="SICAF: An Untitled Arrival" copyright="SICAF, Studio AKA" flv_url="videos/sicaf.mp4" image_url="sicaf.jpg" tags="short"/>
+<tv name="The Cloud Appreciation Society: Morning Glory" copyright="The Cloud Appreciation Society" flv_url="videos/morning_glory.mp4" image_url="morning_glory.jpg" tags="short"/>
+<tv name="Chas Mollet: Seven Ears" copyright="Chas Mollet, Anthony Dickenson" flv_url="videos/seven_ears.mp4" image_url="seven_ears.jpg" tags="music"/>
+<tv name="Setpixel: Retrofabulous" copyright="Charles Forman" flv_url="videos/setpixel.mp4" image_url="setpixel.jpg" tags="technology"/>
+<tv name="Street Lamp" copyright="Aleksandra Domanovic" flv_url="videos/street_lamp.mp4" image_url="street_lamp.jpg" tags="weird"/>
+<tv name="Daniel Rozin: Wooden Mirror" copyright="Daniel Rozin" flv_url="videos/wooden_mirror.mp4" image_url="wooden_mirror.jpg" tags="technology"/>
+<tv name="Ronald McDonald" copyright="kikshin" flv_url="videos/ronald_mcdonald.mp4" image_url="ronald_mcdonald.jpg" tags="subversive"/>
+<tv name="Mark Ronson: Just (feat. Alex Greenwald)" copyright="Mark Ronson, Radiohead" flv_url="videos/mark_ronson_just.mp4" image_url="mark_ronson_just.jpg" tags="music"/>
+<tv name="Roundabout" copyright="Pro X Fade" flv_url="videos/roundabout.mp4" image_url="roundabout.jpg" tags="short"/>
+<tv name="Lumasolator" copyright="PIPS:lab" flv_url="videos/lumasolator.mp4" image_url="lumasolator.jpg" tags="technology"/>
+<tv name="Four Tet: Smile Around the Face" copyright="Four Tet" flv_url="videos/smile_face.mp4" image_url="smile_face.jpg" tags="music"/>
+<tv name="Embedded" copyright="Mark Jenkins" flv_url="videos/embedded.mp4" image_url="embedded.jpg" tags="technology"/>
+<tv name="LA Lets Be Friends" copyright="Three Legged Legs" flv_url="videos/la_lets_be_friends.mp4" image_url="la_lets_be_friends.jpg" tags="short"/>
+<tv name="Graffiti Research Lab: LED Throwies" copyright="Graffiti Research Lab" flv_url="videos/led_throwies.mp4" image_url="led_throwies.jpg" tags="technology"/>
+<tv name="The Music Animation Machine: Clair de Lune" copyright="The Music Animation Machine" flv_url="videos/musanim.mp4" image_url="musanim.jpg" tags="music"/>
+<tv name="Broken" copyright="Alisa McRonald" flv_url="videos/Broken.mp4" image_url="Broken.jpg" tags="weird"/>
+<tv name="Måns Swanberg: Ge-996" copyright="Måns Swanberg" flv_url="videos/ge996.mp4" image_url="ge996.jpg" tags="short"/>
+<tv name="Heliodisplay" copyright="IO2 Technology" flv_url="videos/heliodisplay.mp4" image_url="heliodisplay.jpg" tags="technology"/>
+<tv name="Elbow: The Stops" copyright="Elbow" flv_url="videos/elbow_stops.mp4" image_url="elbow_stops.jpg" tags="music"/>
+<tv name="The Manual Input Sessions" copyright="Golan Levin, Zachary Lieberman" flv_url="videos/manual_input.mp4" image_url="manual_input.jpg" tags="technology"/>
+<tv name="Tyger" copyright="Guilherme Marcondes" flv_url="videos/tyger.mp4" image_url="tyger.jpg" tags="short"/>
+<tv name="The Hell's Kitchen" copyright="Ben Bocquelet" flv_url="videos/hells_kitchen.mp4" image_url="hells_kitchen.jpg" tags="short"/>
+<tv name="Partisan Jab: Ashcroft Fear Remix" copyright="Partisan Jab" flv_url="videos/ashcroft_fear.mp4" image_url="ashcroft_fear.jpg" tags="subversive"/>
+<tv name="Wintergreen: When I Wake Up" copyright="Wintergreen" flv_url="videos/wintergreen_wake_up.mp4" image_url="wintergreen_wake_up.jpg" tags="music"/>
+<tv name="Infernoptix: Digital Pyrotechnic Matrix" copyright="Infernoptix" flv_url="videos/infernoptix.mp4" image_url="infernoptix.jpg" tags="technology"/>
+<tv name="The Knife: Handy-Man" copyright="The Knife" flv_url="videos/handy_man.mp4" image_url="handy_man.jpg" tags="music"/>
+<tv name="InspireMe.tv: Craig Swann" copyright="Brendan Dawes, Craig Swann" flv_url="videos/craig_swann.mp4" image_url="craig_swann.jpg" tags="subversive"/>
+<tv name="Pjotro: The Man with the Musical Suit" copyright="Per Stenius" flv_url="videos/Pjotro.mp4" image_url="Pjotro.jpg" tags="technology"/>
+<tv name="1st Ave Machine: Sixes Last" copyright="1stAveMachine, Alias" flv_url="videos/first_ave.mp4" image_url="first_ave.jpg" tags="short"/>
+<tv name="DJ Format: 3 Feet Deep" copyright="DJ Format" flv_url="videos/three_feet_deep.mp4" image_url="three_feet_deep.jpg" tags="music"/>
+<tv name="Happy Christmas" copyright="Three Legged Legs" flv_url="videos/Happy_Christmas.mp4" image_url="Happy_Christmas.jpg" tags="weird"/>
+<tv name="Conehead" copyright="Don Pan" flv_url="videos/conehead.mp4" image_url="conehead.jpg" tags="short"/>
+<tv name="The Chemical Brothers: Believe" copyright="The Chemical Brothers" flv_url="videos/chemical_believe.mp4" image_url="chemical_believe.jpg" tags="music"/>
+<tv name="Audiopad" copyright="James Patten" flv_url="videos/audiopad.mp4" image_url="audiopad.jpg" tags="technology"/>
+<tv name="Jamie Lidell: New Me" copyright="Jamie Lidell" flv_url="videos/lidell_new_me.mp4" image_url="lidell_new_me.jpg" tags="music"/>
+<tv name="Bim Bam Boom" flv_url="videos/bim_bam_boom.mp4" image_url="bim_bam_boom.jpg" tags="weird"/>
+<tv name="Lost Babies" copyright="goodygoodberg" flv_url="videos/lost-babies.flv" image_url="lost-babies.png" tags="weird"/>
+<tv name="ITV Teachers" flv_url="videos/itv-teachers.flv" image_url="itv-teachers.png" tags="weird"/>
+<tv name="Useless Superpower" copyright="Kane Pickerill" flv_url="videos/Useless_Superpower.mp4" image_url="Useless_Superpower.jpg" tags="weird"/>
+<tv name="Bells Dubstep" copyright="Alex K" flv_url="videos/bells_dubstep.mp4" image_url="bells_dubstep.jpg" tags="weird"/>
+<tv name="Hungry" flv_url="videos/Hungry.mp4" image_url="Hungry.jpg" tags="weird"/>
+<tv name="Melt" flv_url="videos/Melt.mp4" image_url="Melt.jpg" tags="weird"/>
+<tv name="Diarrhea" flv_url="videos/diarrhea.mp4" image_url="diarrhea.jpg" tags="weird"/>
+<tv name="Jacob and the Giraffe" copyright="Cool 3D World" flv_url="videos/jacob_Giraffe.mp4" image_url="jacob_Giraffe.jpg" tags="weird"/>
+<tv name="Hand DJ" copyright="Leo Petersen" flv_url="videos/Hand_DJ.mp4" image_url="Hand_DJ.jpg" tags="weird"/>
+<tv name="Squirrel Monster" copyright="Bortusk Leer" flv_url="videos/squirrel_monster.mp4" image_url="squirrel_monster.jpg" tags="weird"/>
+<tv name="Eegah" flv_url="videos/Eegah.mp4" image_url="Eegah.jpg" tags="weird"/>
+<tv name="Devil Girl" flv_url="videos/devil_girl.mp4" image_url="devil_girl.jpg" tags="weird"/>
+<tv name="Tinker Tailor" copyright="Richard Ayoade" flv_url="videos/Tinker_Tailor.mp4" image_url="Tinker_Tailor.jpg" tags="weird"/>
+<tv name="Objectification" copyright="Albert Omoss" flv_url="videos/Objectification.mp4" image_url="Objectification.jpg" tags="weird"/>
+<tv name="Guacamole" flv_url="videos/Guacamole.mp4" image_url="Guacamole.jpg" tags="weird"/>
+<tv name="Furby" copyright="Mike Diva" flv_url="videos/furby.mp4" image_url="furby.jpg" tags="weird"/>
+<tv name="Jacko Cat" flv_url="videos/jacko_cat.mp4" image_url="jacko_cat.jpg" tags="weird"/>
+<tv name="One Trick Pony" copyright="Punches Bears" flv_url="videos/One_Trick_Pony.mp4" image_url="One_Trick_Pony.jpg" tags="weird"/>
+<tv name="Germs" copyright="Rachel Maclean" flv_url="videos/Germs.mp4" image_url="Germs.jpg" tags="weird"/>
+<tv name="Someone Here?" copyright="Ryan Trecartin" flv_url="videos/someone_here.mp4" image_url="someone_here.jpg" tags="weird"/>
+<tv name="Bomb" copyright="David O'reilly" flv_url="videos/Bomb.mp4" image_url="Bomb.jpg" tags="weird"/>
+<tv name="Motorcycle" copyright="Fenslerfilm" flv_url="videos/motorcycle.mp4" image_url="motorcycle.jpg" tags="weird"/>
+<tv name="Washing Machine" copyright="Aussie50" flv_url="videos/Washing_Machine.mp4" image_url="Washing_Machine.jpg" tags="weird"/>
+<tv name="Sonar Dog" copyright="Sonar Festival" flv_url="videos/sonar_dog.mp4" image_url="sonar_dog.jpg" tags="weird"/>
+<tv name="Hamsters" flv_url="videos/Hamsters.mp4" image_url="Hamsters.jpg" tags="weird"/>
+<tv name="Death Koala" copyright="Jeremy Dower" flv_url="videos/death_koala.mp4" image_url="death_koala.jpg" tags="weird"/>
+<tv name="Let's Get It On" copyright="Seb Mayer" flv_url="videos/Lets_get_It_On.mp4" image_url="Lets_get_It_On.jpg" tags="weird"/>
+<tv name="Skifcha" copyright="Denis Pisarev" flv_url="videos/skifcha.mp4" image_url="skifcha.jpg" tags="weird"/>
+<tv name="Helvetica Scenario" flv_url="videos/Helvetica_Scenario.mp4" image_url="Helvetica_Scenario.jpg" tags="weird"/>
+<tv name="Internal Conflict" copyright="Soft Citizen" flv_url="videos/Internal_Conflict.mp4" image_url="Internal_Conflict.jpg" tags="weird"/>
+<tv name="Weird Dog" copyright="Jeremy Dower" flv_url="videos/Weird_Dog.mp4" image_url="Weird_Dog.jpg" tags="weird"/>
+<tv name="Laughing Owl" copyright="VanCityVideo" flv_url="videos/laughing_owl.mp4" image_url="laughing_owl.jpg" tags="weird"/>
+<tv name="Aerobic" flv_url="videos/Aerobic.mp4" image_url="Aerobic.jpg" tags="weird"/>
+<tv name="Jurrasic Horse" copyright="The Horse Guru" flv_url="videos/jurrasic_horse.mp4" image_url="jurrasic_horse.jpg" tags="weird"/>
+<tv name="Late For Meeting" copyright="David Lewandowski" flv_url="videos/late_for_meeting.mp4" image_url="late_for_meeting.jpg" tags="weird"/>
+<tv name="Armpit Hair" copyright="Ryan Trecartin" flv_url="videos/armpit_hair.mp4" image_url="armpit_hair.jpg" tags="weird"/>
+<tv name="Wolf Spirit" copyright="Jimmy Joe Roche" flv_url="videos/Wolf_Spirit.mp4" image_url="Wolf_Spirit.jpg" tags="weird"/>
+<tv name="Adhisaya Piravi" flv_url="videos/adhisaya_piravi.mp4" image_url="adhisaya_piravi.jpg" tags="weird"/>
+<tv name="Club Of Friends" copyright="Calvert 22" flv_url="videos/club_of_friends.mp4" image_url="club_of_friends.jpg" tags="weird"/>
+<tv name="Sand Man" copyright="Tatered" flv_url="videos/sand_man.mp4" image_url="sand_man.jpg" tags="weird"/>
+<tv name="Peek-a-Boo" copyright="Big Marvel" flv_url="videos/Peek_a_boo.mp4" image_url="Peek_a_boo.jpg" tags="weird"/>
+<tv name="Goats" copyright="Bill Wurtz" flv_url="videos/Goats.mp4" image_url="Goats.jpg" tags="weird"/>
+
+
+</listings>
